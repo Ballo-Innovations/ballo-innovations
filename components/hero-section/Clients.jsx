2e3cf2bdad7a7bdb2ce8d5b53b972e5824398fe7
c00p75 <georgecoopmsapenda@gmail.com>
2024-04-05 09:23:45 +0200

    Add hero section

---
+++ components/hero-section/Clients.jsx
@@ -0,0 +1,30 @@
+import clientLogos from "@/constants/clientLogos"
+import Image from "next/image"
+
+const Clients = () => {
+  return (
+    <div>
+      <h2 className="hero-subheading mt-16 mb-10 px-5 md:px-32">
+        Clients & Partners
+      </h2>
+
+      <div className="logos">
+        <div className="logos-slide flex w-max gap-2">
+          {clientLogos.map((logo) => {
+            return(
+              <div key={logo.name} style={{ width: `${logo.width}em`}} className="flex-center">
+                <Image
+                  src={logo.img}
+                  alt={logo.name}
+                  quality={100}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Clients
