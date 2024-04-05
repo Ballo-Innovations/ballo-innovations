c80542de129d3c99c9b59daa9d72b66a5846a851
c00p75 <georgecoopmsapenda@gmail.com>
2024-04-05 09:21:32 +0200

    Update layout

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/components/hero-section/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Ballo Innovation</h1>
+    <main className="flex min-h-screen">
+      <Hero />
     </main>
   );
 }
