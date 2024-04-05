0799263fe89a490fa1d55c237226a40fe8e5ee4c
c00p75 <georgecoopmsapenda@gmail.com>
2024-04-05 09:24:17 +0200

    Add constants

---
+++ constants/clientLogos.js
@@ -0,0 +1,77 @@
+import makhulu from '@/public/Client Logos/Makhulu High Res Logo white.png'
+import paramountLogistics from '@/public/Client Logos/paramount-1 white.png'
+import omphile from '@/public/Client Logos/Omphile-White.png'
+import insizwe from '@/public/Client Logos/9.png'
+import mudenda from '@/public/Client Logos/Mudenda Capital Logo to send-03.png'
+import fi from '@/public/Client Logos/Financial Insights Logo white.png'
+import tinge from '@/public/Client Logos/Tinge logo white.png'
+import iv from '@/public/Client Logos/iv1.png'
+import zhtc from '@/public/Client Logos/logo-2 white.png'
+import seneca from '@/public/Client Logos/seneca-logo dsc-02.png'
+import shane from '@/public/Client Logos/Shane Investments logo.png'
+import swr from '@/public/Client Logos/SWR Logo white.png'
+
+const clientLogos = [
+  {
+    name: 'Makhulu Investments',
+    img: makhulu,
+    width: 14,
+  },
+  {
+    name: 'Paramount Logistics',
+    img: paramountLogistics,
+    width: 17,
+  },
+  {
+    name: 'Omphile',
+    img: omphile,
+    width: 6.5,
+  },
+  {
+    name: 'Insizwe Insurance',
+    img: insizwe,
+    width: 11,
+  },
+  {
+    name: 'Mudenda Capital',
+    img: mudenda,
+    width: 13,
+  },
+  {
+    name: 'Financial Insights',
+    img: fi,
+    width: 7,
+  },
+  {
+    name: 'Tinge',
+    img: tinge,
+    width: 9.5,
+  },
+  {
+    name: 'The IV Lounge',
+    img: iv,
+    width: 10,
+  },
+  {
+    name: 'ZHTC',
+    img: zhtc,
+    width: 13,
+  },
+  {
+    name: 'Seneca',
+    img: seneca,
+    width: 18,
+  },
+  {
+    name: 'Shane Investments',
+    img: shane,
+    width: 8,
+  },
+  {
+    name: 'Style with RoseS',
+    img: swr,
+    width: 10,
+  }
+];
+
+export default clientLogos;
---
+++ constants/services.js
@@ -0,0 +1,29 @@
+import digital_marketing from '@/public/elements small/3.png'
+import social_media from '@/public/elements small/4.png'
+import web_app_development from '@/public/elements small/5.png'
+import data_analysis from '@/public/elements small/8.png'
+
+const services = [
+  {
+    name: 'Digital Marketing',
+    img: digital_marketing,
+    summary: 'We involve researching and analysing consumer behaviour and market trends to develop effective strategies for promoting and selling products or services.'
+  },
+  {
+    name: 'Social Media Management',
+    img: social_media,
+    summary: 'Our exemplary social media team is ordinarily responsible for developing strategies to increase followers, create and oversee social campaigns, produce content, review analytics, and communicate with key stakeholders in a company'
+  },
+  {
+    name: 'Web & App Development',
+    img: web_app_development,
+    summary: 'Advertising refers to the process of creating and distributing messages or content to promote your product, service, or brand to your target audience.'
+  },
+  {
+    name: 'Data Analytics',
+    img: data_analysis,
+    summary: 'Data analytics (DA) is done with the aid of specialized systems and software. Our data analytics techniques are widely used in commercial industries to enable organizations to make more-informed business decisions.'
+  },
+]
+
+export default services;
