1e0949d5e1296b7db53dcbebacedd70755770b6b
c00p75 <georgecoopmsapenda@gmail.com>
2024-04-17 09:32:00 +0200

    Complete Ballo Adds Demo section

--- components/homepage/balloAds/BalloAdsDemo.jsx
+++ components/homepage/balloAds/BalloAdsDemo.jsx
@@ -2,11 +2,12 @@ import './balloAds.css'
 import background from '@/public/Backgrounds/hero-bg.png'
 // NOTE: Use the image bellow to animate effects
 // import balloAds from '@/public/elements small/1.png'
-
+// import playStore from '@/public/elements small/18.png'
+// import appleStore from '@/public/elements small/19.png'
 
 import balloAdsPhone from '@/public/elements small/1.png'
-import playStore from '@/public/elements small/18.png'
-import appleStore from '@/public/elements small/19.png'
+import appIcon from '@/public/Ballo Logo New/App icon 1.png'
+
 import Image from 'next/image'
 
 const BalloAdsDemo = () => {
@@ -16,11 +17,17 @@ const BalloAdsDemo = () => {
       style={{ background: `url(${background.src})`, backgroundSize: 'cover' }}
     >
       <div className="flex-center flex-col md:flex-row md:px-10 gap-10 md:gap-0">
-        <div className='flex-center flex-col text-center gap-10'>
-          <h2 className='subheading mb-5 leading-[0.9em] md:text-[4.5em]'>
+        <div className='flex justify-center items-center md:items-start flex-col text-center gap-10'>
+          <Image
+            src={appIcon}
+            alt="Ballo Ads"
+            quality={100}
+            className="w-28"
+          />
+          <h2 className='subheading mb-5 leading-[0.9em] md:text-[4.5em] md:text-left'>
             Advertise your brand in only a few seconds.
           </h2>
-          <button className='hidden md:block rounded-full px-10 py-2 bg-[var(--brand-color-4)] text-[var(--brand-color-1)] font-semibold text-2xl'>
+          <button className='hidden md:block rounded-full px-10 py-2 bg-[var(--brand-color-4)] text-[var(--brand-color-1)] font-semibold text-xl'>
             Coming Soon
           </button>
         </div>
