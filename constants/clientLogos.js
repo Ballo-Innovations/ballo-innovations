53817d04b9bf3affdeeb2b9bc431ec6975771dba
c00p75 <georgecoopmsapenda@gmail.com>
2024-07-13 00:18:06 +0200

    Add Bayport to Our Clients section

--- constants/clientLogos.js
+++ constants/clientLogos.js
@@ -1,77 +1,84 @@
-import makhulu from '@/public/Client Logos/Makhulu High Res Logo white.png'
-import paramountLogistics from '@/public/Client Logos/paramount-1 white.png'
-import omphile from '@/public/Client Logos/Omphile-White.png'
-import insizwe from '@/public/Client Logos/9.png'
-import mudenda from '@/public/Client Logos/Mudenda Capital Logo to send-03.png'
-import fi from '@/public/Client Logos/Financial Insights Logo white.png'
-import tinge from '@/public/Client Logos/Tinge logo white.png'
-import iv from '@/public/Client Logos/iv1.png'
-import zhtc from '@/public/Client Logos/logo-2 white.png'
-import seneca from '@/public/Client Logos/seneca-logo dsc-02.png'
-import shane from '@/public/Client Logos/Shane Investments logo.png'
-import swr from '@/public/Client Logos/SWR Logo white.png'
+import makhulu from "@/public/Client Logos/Makhulu High Res Logo white.png";
+import paramountLogistics from "@/public/Client Logos/paramount-1 white.png";
+import omphile from "@/public/Client Logos/Omphile-White.png";
+import insizwe from "@/public/Client Logos/9.png";
+import mudenda from "@/public/Client Logos/Mudenda Capital Logo to send-03.png";
+import fi from "@/public/Client Logos/Financial Insights Logo white.png";
+import tinge from "@/public/Client Logos/Tinge logo white.png";
+import iv from "@/public/Client Logos/iv1.png";
+import zhtc from "@/public/Client Logos/logo-2 white.png";
+import seneca from "@/public/Client Logos/seneca-logo dsc-02.png";
+import shane from "@/public/Client Logos/Shane Investments logo.png";
+import swr from "@/public/Client Logos/SWR Logo white.png";
+import bayport from "@/public/Client Logos/bayport.png";
 
 const clientLogos = [
   {
-    name: 'Makhulu Investments',
+    name: "Makhulu Investments",
     img: makhulu,
     width: 14,
   },
+
   {
-    name: 'Paramount Logistics',
+    name: "Paramount Logistics",
     img: paramountLogistics,
     width: 17,
   },
   {
-    name: 'Omphile',
+    name: "Omphile",
     img: omphile,
     width: 6.5,
   },
   {
-    name: 'Insizwe Insurance',
+    name: "Insizwe Insurance",
     img: insizwe,
     width: 11,
   },
   {
-    name: 'Mudenda Capital',
+    name: "Mudenda Capital",
     img: mudenda,
     width: 13,
   },
   {
-    name: 'Financial Insights',
+    name: "Financial Insights",
     img: fi,
     width: 7,
   },
   {
-    name: 'Tinge',
+    name: "Tinge",
     img: tinge,
     width: 9.5,
   },
   {
-    name: 'The IV Lounge',
+    name: "Bayport",
+    img: bayport,
+    width: 14,
+  },
+  {
+    name: "The IV Lounge",
     img: iv,
     width: 10,
   },
   {
-    name: 'ZHTC',
+    name: "ZHTC",
     img: zhtc,
     width: 13,
   },
   {
-    name: 'Seneca',
+    name: "Seneca",
     img: seneca,
     width: 18,
   },
   {
-    name: 'Shane Investments',
+    name: "Shane Investments",
     img: shane,
     width: 8,
   },
   {
-    name: 'Style with RoseS',
+    name: "Style with RoseS",
     img: swr,
     width: 10,
-  }
+  },
 ];
 
 export default clientLogos;
