06a710bc41b58f0887d790024a9c5d3a61822b58
c00p75 <georgecoopmsapenda@gmail.com>
2024-07-23 11:45:19 +0200

    Adjust image ratios

--- components/works page/GraphicDesignPage/index.jsx
+++ components/works page/GraphicDesignPage/index.jsx
@@ -1,11 +1,10 @@
 "use client";
 
-import phone from "@/public/elements small/phone.png";
-import Image from "next/image";
 import { graphicDesignWorks } from "@/constants/works";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 import { useCallback, useState } from "react";
+import "./style.css";
 
 const GraphicDesignPage = () => {
   const [currentImage, setCurrentImage] = useState(0);
@@ -21,21 +20,8 @@ const GraphicDesignPage = () => {
     setViewerIsOpen(false);
   };
 
-  const photos = [
-    {
-      src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
-      width: 4,
-      height: 3,
-    },
-    {
-      src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
-      width: 1,
-      height: 1,
-    },
-  ];
-
   return (
-    <section className="pt-[30vh] bg-[var(--brand-color-5)] blue-black-bg-grandient-2">
+    <section className="pt-[30vh] pb-10 bg-[var(--brand-color-5)] blue-black-bg-grandient-2">
       <div className="flex flex-col">
         <div className="md:px-32 text-white text-center lg:text-start">
           <h1 className="subheading">Our Works</h1>
@@ -43,7 +29,7 @@ const GraphicDesignPage = () => {
           <h2 className="text-3xl font-bold my-6">Graphic Design</h2>
         </div>
 
-        <div>
+        <div id="graphics-gallery">
           <Gallery photos={graphicDesignWorks} onClick={openLightbox} />
           <ModalGateway>
             {viewerIsOpen ? (
--- constants/works.js
+++ constants/works.js
@@ -1,47 +1,47 @@
-import graphic_1 from "@/public/projects/graphic-design/1.png";
-import graphic_2 from "@/public/projects/graphic-design/2.png";
-import graphic_3 from "@/public/projects/graphic-design/3.png";
-import graphic_4 from "@/public/projects/graphic-design/4.png";
+import workers_day from "@/public/projects/graphic-design/1.png";
+import eu from "@/public/projects/graphic-design/2.png";
+import debbie_in_debt from "@/public/projects/graphic-design/3.png";
+import iv_winter_tips from "@/public/projects/graphic-design/4.png";
 import graphic_5 from "@/public/projects/graphic-design/5.png";
-import graphic_6 from "@/public/projects/graphic-design/6.png";
-import graphic_7 from "@/public/projects/graphic-design/7.png";
+import mining_indaba from "@/public/projects/graphic-design/6.png";
+import swr_community_building from "@/public/projects/graphic-design/7.png";
 import graphic_8 from "@/public/projects/graphic-design/8.png";
-import graphic_9 from "@/public/projects/graphic-design/9.png";
-import graphic_10 from "@/public/projects/graphic-design/10.png";
-import graphic_11 from "@/public/projects/graphic-design/11.png";
-import graphic_12 from "@/public/projects/graphic-design/12.png";
+import insizswe_experience_security from "@/public/projects/graphic-design/9.png";
+import fiz_tourism_show from "@/public/projects/graphic-design/10.png";
+import swr_congrats from "@/public/projects/graphic-design/11.png";
+import iv_starts_with_within from "@/public/projects/graphic-design/12.png";
 import graphic_13 from "@/public/projects/graphic-design/13.png";
-import graphic_14 from "@/public/projects/graphic-design/14.png";
-import graphic_15 from "@/public/projects/graphic-design/15.png";
-import graphic_16 from "@/public/projects/graphic-design/16.png";
-import graphic_17 from "@/public/projects/graphic-design/17.png";
-import graphic_18 from "@/public/projects/graphic-design/18.jpg";
-import graphic_19 from "@/public/projects/graphic-design/19.jpg";
-import graphic_20 from "@/public/projects/graphic-design/20.jpg";
-import graphic_21 from "@/public/projects/graphic-design/21.jpg";
-import graphic_22 from "@/public/projects/graphic-design/22.jpg";
+import mudenda_learning from "@/public/projects/graphic-design/14.png";
+import fiz_chess from "@/public/projects/graphic-design/15.png";
+import insizwe_motor_insurance from "@/public/projects/graphic-design/16.png";
+import insizwe_med from "@/public/projects/graphic-design/17.png";
+import discover_van_front from "@/public/projects/graphic-design/18.jpg";
+import discover_van_back from "@/public/projects/graphic-design/19.jpg";
+import bayport_banking from "@/public/projects/graphic-design/20.jpg";
+import bayport_time_is_money from "@/public/projects/graphic-design/21.jpg";
+import grow_your_future from "@/public/projects/graphic-design/22.jpg";
 
 export const graphicDesignWorks = [
-  { src: graphic_22.src, width: 8, height: 5 },
-  { src: graphic_1.src, width: 5, height: 4 },
-  { src: graphic_3.src, width: 10, height: 10 },
-  { src: graphic_4.src, width: 1, height: 1 },
-  { src: graphic_5.src, width: 1, height: 1 },
-  { src: graphic_6.src, width: 10, height: 9 },
-  { src: graphic_7.src, width: 6, height: 7 },
+  { src: grow_your_future.src, width: 3, height: 2 },
+  { src: workers_day.src, width: 9, height: 7 },
+  { src: discover_van_front.src, width: 3, height: 4 },
+  { src: insizswe_experience_security.src, width: 3, height: 2 },
+  { src: bayport_time_is_money.src, width: 6, height: 4 },
+  { src: graphic_5.src, width: 10, height: 9 },
+  { src: swr_community_building.src, width: 6, height: 4 },
   { src: graphic_8.src, width: 1, height: 1 },
-  { src: graphic_9.src, width: 1, height: 1 },
-  { src: graphic_10.src, width: 1, height: 1 },
-  { src: graphic_11.src, width: 1, height: 1 },
-  { src: graphic_12.src, width: 6, height: 4 },
+  { src: fiz_tourism_show.src, width: 4, height: 3 },
+  { src: iv_starts_with_within.src, width: 1, height: 1 },
+  { src: mudenda_learning.src, width: 4, height: 3 },
+  { src: debbie_in_debt.src, width: 1, height: 1 },
+  { src: discover_van_back.src, width: 1, height: 1 },
+  { src: mining_indaba.src, width: 10, height: 9 },
+  { src: insizwe_med.src, width: 3, height: 2 },
+  { src: insizwe_motor_insurance.src, width: 10, height: 8 },
+  { src: fiz_chess.src, width: 1, height: 1 },
+  { src: bayport_banking.src, width: 5, height: 4 },
+  { src: swr_congrats.src, width: 1, height: 1 },
+  { src: iv_winter_tips.src, width: 5, height: 4 },
+  { src: eu.src, width: 1, height: 1 },
   { src: graphic_13.src, width: 1, height: 1 },
-  { src: graphic_14.src, width: 1, height: 1 },
-  { src: graphic_15.src, width: 10, height: 8 },
-  { src: graphic_16.src, width: 1, height: 1 },
-  { src: graphic_17.src, width: 1, height: 1 },
-  { src: graphic_18.src, width: 1, height: 1 },
-  { src: graphic_19.src, width: 10, height: 7 },
-  { src: graphic_20.src, width: 1, height: 1 },
-  { src: graphic_2.src, width: 1, height: 1 },
-  { src: graphic_21.src, width: 1, height: 1 },
 ];
