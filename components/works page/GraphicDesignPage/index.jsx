5e879467e887754ac669c0da7cbf1be360c8e1b9
c00p75 <georgecoopmsapenda@gmail.com>
2024-07-30 21:46:52 +0200

    Add gallery grid

--- components/works page/GraphicDesignPage/index.jsx
+++ components/works page/GraphicDesignPage/index.jsx
@@ -2,7 +2,7 @@ import ImageGallery from "./Image Gallery";
 
 const GraphicDesignPage = () => {
   return (
-    <section className="pt-[30vh] pb-10 bg-[var(--brand-color-5)] blue-black-bg-grandient-2 md:pl-9 md:pr-15">
+    <section className="pt-[30vh] sm:pb-10 bg-[var(--brand-color-5)] blue-black-bg-grandient-2 md:pl-9 md:pr-15">
       <div className="flex flex-col">
         <div className="md:px-32 text-white text-center lg:text-start">
           <h1 className="font-bold text-8xl">Our Works</h1>
