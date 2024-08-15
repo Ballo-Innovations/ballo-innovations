5f6ea2260692fd1a606a38dae7016658281c987b
c00p75 <georgecoopmsapenda@gmail.com>
2024-08-15 13:05:36 +0200

    Add write ups

--- components/works page/categories/categoryLinks/CategoryLinks.jsx
+++ components/works page/categories/categoryLinks/CategoryLinks.jsx
@@ -5,28 +5,28 @@ import "./style.css";
 
 const CategoryLinks = ({ category }) => {
   return (
-    <div className="flex-center flex-col md:w-[30%] mt-10 category-card-container px-3">
+    <div className="flex-center flex-col lg:w-[30%] mt-10 category-card-container px-3">
       <Link
         href={`${category.path}`}
         className={`flex-center ${
           category.name === "Social Media Management" ? "w-[45%]" : ""
         } ${category.name === "Advertising" ? "w-2/3" : ""} ${
-          category.name === "Graphic Design" ? "w-4/5 md:mr-10" : ""
+          category.name === "Graphic Design" ? "w-4/5 lg:mr-10" : ""
         } ${
           category.name === "Photography" ? "w-4/5" : ""
-        } md:w-[30vw] md:h-[50vh] overflow-visible`}
+        } lg:w-[30vw] lg:h-[50vh] overflow-visible`}
       >
         <Image
           src={category.cover}
           alt="Mission and Values"
           quality={100}
-          className="md:-ml-10 w-[80vh] md:w-auto h-full object-contain overflow-visible category-cover-img"
+          className="lg:-ml-10 w-[80vh] lg:w-auto h-full object-contain overflow-visible category-cover-img"
         />
       </Link>
 
       <Link
         href={`${category.path}`}
-        className="flex relative gap-5 mt-3 ml-10 md:ml-0"
+        className="flex relative gap-5 mt-3 ml-10 lg:ml-0"
       >
         <h2 className="font-bold text-3xl break-words w-min text-center">
           {category.name}
@@ -36,7 +36,7 @@ const CategoryLinks = ({ category }) => {
             src={arrow}
             alt={category.name}
             quality={100}
-            className="w-10 md:opacity-0 lg:opacity-100"
+            className="w-10 lg:opacity-0 lg:opacity-100"
           />
         </span>
       </Link>
