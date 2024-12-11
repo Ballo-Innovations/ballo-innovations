01d8029b382f3c0e0f4cb59101e745cb02282101
c00p75 <georgecoopmsapenda@gmail.com>
2024-12-11 16:05:33 +0200

    REstore site

--- components/loader/index.jsx
+++ components/loader/index.jsx
@@ -7,110 +7,82 @@ import logoSm from "@/public/Ballo Logo New/svg/Ballo logo new-02.svg";
 import FollowMouse from "./mouseTracker";
 import RippleEffect from "./rippleEffect";
 import { useEffect, useState } from "react";
-import ring from "@/public/elements small/15.png";
 
 const Loader = () => {
   const [showLoader, setShowLoader] = useState(true);
   const [fadeOut, setFadeOut] = useState(false);
-  const [animation, setAnimation] = useState(false);
 
   useEffect(() => {
+    // Set timeouts and store the IDs
     const fadeOutTimeout = setTimeout(() => {
-      setAnimation(true);
-    }, 1000);
-  });
+      document.querySelector("body")?.classList.remove("no-scroll");
+      setFadeOut(true);
+    }, 500);
 
-  // useEffect(() => {
-  //   // Set timeouts and store the IDs
-  //   const fadeOutTimeout = setTimeout(() => {
-  //     document.querySelector("body")?.classList.remove("no-scroll");
-  //     setFadeOut(true);
-  //   }, 500);
+    const hideLoaderTimeout = setTimeout(() => {
+      setShowLoader(false);
+    }, 500);
 
-  //   const hideLoaderTimeout = setTimeout(() => {
-  //     setShowLoader(false);
-  //   }, 500);
-
-  //   // Clear timeouts on component unmount
-  //   return () => {
-  //     clearTimeout(fadeOutTimeout);
-  //     clearTimeout(hideLoaderTimeout);
-  //   };
-  // }, []);
+    // Clear timeouts on component unmount
+    return () => {
+      clearTimeout(fadeOutTimeout);
+      clearTimeout(hideLoaderTimeout);
+    };
+  }, []);
 
   return (
-    // showLoader && (
-    //   <div id="loader" className={`${fadeOut && "opacity-0"}`}>
-    //     {/* <FollowMouse unmount={fadeOut} /> */}
-    //     <RippleEffect
-    //       top={"10%"}
-    //       left={"-20%"}
-    //       width={"20vw"}
-    //       rippleClass="ripple-1"
-    //     />
+    showLoader && (
+      <div id="loader" className={`${fadeOut && "opacity-0"}`}>
+        {/* <FollowMouse unmount={fadeOut} /> */}
+        <RippleEffect
+          top={"10%"}
+          left={"-20%"}
+          width={"20vw"}
+          rippleClass="ripple-1"
+        />
 
-    //     <RippleEffect
-    //       top={"70%"}
-    //       left={"50%"}
-    //       width={"8vw"}
-    //       rippleClass="ripple-2"
-    //     />
+        <RippleEffect
+          top={"70%"}
+          left={"50%"}
+          width={"8vw"}
+          rippleClass="ripple-2"
+        />
 
-    //     <RippleEffect
-    //       top={"-15%"}
-    //       left={"40%"}
-    //       width={"11vw"}
-    //       rippleClass="ripple-3"
-    //     />
+        <RippleEffect
+          top={"-15%"}
+          left={"40%"}
+          width={"11vw"}
+          rippleClass="ripple-3"
+        />
 
-    //     <RippleEffect
-    //       top={"20%"}
-    //       left={"70%"}
-    //       width={"13vw"}
-    //       rippleClass="ripple-4"
-    //     />
+        <RippleEffect
+          top={"20%"}
+          left={"70%"}
+          width={"13vw"}
+          rippleClass="ripple-4"
+        />
 
-    //     <div className="loader-container relative w-full h-full flex-center overflow-visible">
-    //       <Image
-    //         src={logo}
-    //         alt="Ballo Innovations"
-    //         quality={100}
-    //         className="w-[80vw] hidden md:block"
-    //         loading="eager"
-    //         blurDataURL={logo.blurhashDataUrl}
-    //       />
-
-    //       <Image
-    //         src={logoSm}
-    //         alt="Ballo Innovations"
-    //         quality={100}
-    //         className="w-[80vw] block md:hidden"
-    //         loading="eager"
-    //         blurDataURL={logo.blurhashDataUrl}
-    //       />
-    //     </div>
-    //   </div>
-    // )
-    <div className="reel-wrapper-wrapper">
-      <div className="reel-wraper">
-        <button class="button flex" data-text="Awesome">
+        <div className="loader-container relative w-full h-full flex-center overflow-visible">
           <Image
-            src={ring}
+            src={logo}
             alt="Ballo Innovations"
             quality={100}
-            className={`ring absolute z-30 ${animation && "animate-ring"}`}
+            className="w-[80vw] hidden md:block"
             loading="eager"
+            blurDataURL={logo.blurhashDataUrl}
           />
-          <span class="actual-text">&nbsp;WEBSITE..&nbsp;</span>
-          <span
-            aria-hidden="true"
-            className={`hover-text ${animation && "animate-text"}`}
-          >
-            &nbsp;WEBSITE..&nbsp;
-          </span>
-        </button>
+
+          <Image
+            src={logoSm}
+            alt="Ballo Innovations"
+            quality={100}
+            className="w-[80vw] block md:hidden"
+            loading="eager"
+            blurDataURL={logo.blurhashDataUrl}
+          />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
